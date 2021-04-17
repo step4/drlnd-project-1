4650f440fb695f9a1cea6423f74cfdb7a41dcc5b
Christian Rausch <c.rausch020@gmail.com>
2021-04-17 14:28:05 +0200

    code finished

--- ddqn_agent.py
+++ ddqn_agent.py
@@ -85,8 +85,12 @@ class Agent():
         """
         states, actions, rewards, next_states, dones = experiences
 
-        # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        # Get max predicted Q values (for next states) from local model
+        Q_next_actions = self.qnetwork_local(next_states).detach().argmax(1)
+        
+        # double dqn evaluation with the target model
+        Q_targets_next = self.qnetwork_target(next_states).detach().gather(1,Q_next_actions.unsqueeze(1))
+        
         # Compute Q targets for current states 
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
